d54afee2fd3d5841ff272794745bfe1ee35152ab
Xen0Xys <red57101@gmail.com>
2021-11-09 12:13:21 +0100

    Create basics app methods in app.js Create basic class in commands.js import app.js as a module in index.html

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,8 +1,52 @@
+import {Commands} from "./commands.js";
+
+/**
+ * Main app object, with all app methods
+ * @param nom
+ * @constructor
+ */
 function App(nom) {
-    this.sendUserMessage = function() {
+    // Constructor
+    console.log("Loading app...")
+    this.commands = new Commands();
+    this.messageList = [];
+
+    /**
+     * Method call when user send a message
+     * @param message user message
+     */
+    this.sendUserMessage = function(message) {
+        this.messageList.push(message);
+    };
+
+    /**
+     * Method call when bot send a message
+     * @param message bot message
+     */
+    this.sendBotMessage = function(message) {
 
     };
-    this.sendBotMessage = function() {
 
+    /**
+     * Get message list with filter apply
+     * @param filter Filter String, can be "" for getting all messages
+     * @returns {*[]} List of messages filtered
+     */
+    this.getMessageListWithFilter = function(filter){
+        const filteredMessageList = [];
+        for(let i=0; i<this.messageList.length; i++){
+            if(this.messageList[i].includes(filter)){
+                filteredMessageList.push(this.messageList[i]);
+            }
+        }
+        return filteredMessageList;
     };
+    console.log("App loaded!")
 }
+
+// Debug
+const app = new App();
+app.sendUserMessage("Yolo")
+app.sendUserMessage("Test")
+app.sendUserMessage("oe")
+console.log(app.getMessageListWithFilter(""))
---
+++ www/scripts/commands.js
@@ -0,0 +1,7 @@
+function Commands() {
+    this.parse = function(){
+
+    };
+}
+
+export {Commands};
