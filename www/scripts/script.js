62ec737813a2d36e47d0ff4ed98b02868d711c05
lefoll.lucien@protonmail.ch <e27rWegFtXy8rLS>
2021-11-10 14:22:52 +0100

    Margin

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -2,29 +2,33 @@
 function useMe() {
     switch (document.getElementById("help").style.display) {
         case "none":
-            document.body.style.columnCount = 2;
+            document.body.style.columnCount = "2";
             document.getElementById("help").style.display = "";
+            document.getElementById("chatbox").style.marginLeft = "0%";
             break;
         case "":
             document.body.style.clear;
             document.getElementById("help").style.display = "none";
+            document.getElementById("chatbox").style.marginLeft = "5%";
             break;
         default:
-            document.body.style.columnCount = 2;
+            document.body.style.columnCount = "2";
             document.getElementById("help").style.display = "";
+            document.getElementById("chatbox").style.marginLeft = "0%";
     }
 }
 
 function chat(){
     // crée un nouvel élément div
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
+    let i = 0;
     // et lui donne un peu de contenu
-    var newContent = document.createTextNode('Hi there and greetings!');
+    let newContent = document.createTextNode('Hi there and greetings!');
     // ajoute le nœud texte au nouveau div créé
     newDiv.appendChild(newContent);
     // ajoute une classe au nouveau div crée
     newDiv.classList.add("msg");
     // ajoute le nouvel élément créé et son contenu dans le DOM
-    var currentDiv = document.getElementById('void');
+    let currentDiv = document.getElementById('void');
     document.getElementById("chat").insertBefore(newDiv, currentDiv);
 }
